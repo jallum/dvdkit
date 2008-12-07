2d77815d136fa9eb78adefeef3bdda80c84dfd07
sdickson <sdickson@server.local>
2008-12-07 16:41:12 -0600

    Unit tests Added Test target bundle(OCUnit)

---
+++ DvdTesting.h
@@ -0,0 +1,22 @@
+//
+//  DvdTesting.h
+//  DVDKit
+//
+//  Created by sdickson on 12/7/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DvdTesting : SenTestCase {
+	float f1;
+    float f2;
+	
+	
+}
+
+- (void) testAddition;
+
+@end
+
