49317cac5e03400c340f26242aa7d7e73d8837c1
Jason Allum <jallum@gmail.com>
2008-12-09 10:42:56 -0500

    Added an executable, so that unit tests can be debugged.  Set up the basic framework for Shane to follow for testing DVDCommand.

--- Unit Tests/DvdTesting.m
+++ Unit Tests/DvdTesting.m
@@ -1,40 +1,39 @@
-//
-//  DvdTesting.m
-//  DVDKit
-//
-//  Created by sdickson on 12/7/08.
-//  Copyright 2008 __MyCompanyName__. All rights reserved.
-//
-
 #import "DvdTesting.h"
 #import <DVDKit/DVDKit.h>
 
 @implementation DvdTesting
 
-- (void) testDvd
+- (void) testDecodingAndDescriptions
 {
-    DVDVirtualMachine* vm = [[DVDVirtualMachine alloc] initWithDataSource:self];
-	
-	// 7100000D43210000 | g[13] = 0x4321
-	uint8_t _i00001[] = { 0x71, 0x00, 0x00, 0x0D, 0x43, 0x21, 0x00, 0x00 };
-	DVDCommand* _c00001 = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:_i00001 length:8]];
-	NSString* _s00001 = [_c00001 description];
-	// TODO: Assert that _s00001 == "      7100000d43210000 | g[13] = 0x4321 ("C!")"
-	[_c00001 executeAgainstVirtualMachine:vm];
-	// TODO: Assert that the value of the vm's g13 is 0x4321.
-	
+    DVDCommand* command;
     
-}
+    //  0000000000000000 | Nop
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x00\x00\x00\x00\x00\x00\x00\x00" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      0000000000000000 | Nop"], @"Instruction not decoded properly.");
+    
+    //  000100000000000a | Goto 10
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x00\x01\x00\x00\x00\x00\x00\x0a" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      000100000000000a | Goto 10"], @"Instruction not decoded properly.");
 
-- (void) setUp
-{
-    f1 = 3.0;
-    f2 = 3.0;
-}
+    //  0002000000000000 | Break
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x00\x02\x00\x00\x00\x00\x00\x00" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      0002000000000000 | Break"], @"Instruction not decoded properly.");
 
-- (void) testAddition
-{
-    STAssertTrue (f1 + f2 == 5.0, @"%f + %f should equal 5.0", f1, f2);
+    //  0021000600020007 | if (g[6] == g[2]) Goto 7
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x00\x21\x00\x06\x00\x02\x00\x07" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      0021000600020007 | if (g[6] == g[2]) Goto 7"], @"Instruction not decoded properly.");    
+    
+    //  0041000200050006 | if (g[2] >= g[5]) Goto 6
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x00\x41\x00\x02\x00\x05\x00\x06" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      0041000200050006 | if (g[2] >= g[5]) Goto 6"], @"Instruction not decoded properly.");    
+    
+    //  7100000D43210000 | g[13] = 0x4321
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x71\x00\x00\x0D\x43\x21\x00\x00" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      7100000d43210000 | g[13] = 0x4321 (\"C!\")"], @"Instruction not decoded properly.");
+    
+    //  7100000000000000 | g[0] = 0x0
+	command = [DVDCommand commandWithData:[NSData dataWithBytesNoCopy:"\x71\x00\x00\x00\x00\x00\x00\x00" length:8 freeWhenDone:NO]];
+    STAssertTrue([[command description] isEqualTo:@"      7100000000000000 | g[0] = 0x0"], @"Instruction not decoded properly.");
 }
 
 @end
